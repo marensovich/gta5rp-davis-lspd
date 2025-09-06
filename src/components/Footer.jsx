85ebe3be6209c3673b436518f5de40b5e9a2b32e
marensovich <bushidik228@mail.ru>
2025-09-06 18:54:12 +0000

    Обновлен Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
             Создано игроком <b>Bushido Elusive</b>. Лицензия<a href="https://github.com/marensovich/gta5rp-davis-lspd/blob/master/LICENSE" target="_blank" rel="noopener noreferrer">MIT</a>.
         </div>*/
         <div className="footer-bar">
-            Создано игроком <b>Bushido Elusive</b>.
+            Создано игроком <b>Bushido Elusive</b>. В случае проблем с визуальной частью сайта или предложением по улучшению - обратитесь в личные сообщения <b>@marensov</b> (ДС)
         </div>
     );
 }
