e6c135c635ded58de5b6aa1bfe9a07ca5b3c34a3
marensovich <bushidik228@mail.ru>
2025-09-07 11:37:18 +0000

    Добавлены карточки для всех гос. фракций

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import '../App.css';
 import { useNavigate } from 'react-router-dom';
-import lspdLogo from '../fractions/lspd/img/gta5rp-logo.png';
+import lspdLogo from '../fractions/lspd/img/lspd-logo.jpg';
 
 export default function Home() {
     const navigate = useNavigate();
@@ -13,6 +13,26 @@ export default function Home() {
                 <div className="faction-card" onClick={() => navigate('/lspd')}>
                     <img src={lspdLogo} alt="LSPD" className="faction-logo" />
                     <p className="faction-name">LSPD</p>
+                </div>
+                <div className="faction-card" onClick={() => navigate('/lspd')}>
+                    <img src={lspdLogo} alt="LSSD" className="faction-logo" />
+                    <p className="faction-name">LSSD</p>
+                </div>
+                <div className="faction-card" onClick={() => navigate('/lspd')}>
+                    <img src={lspdLogo} alt="FIB" className="faction-logo" />
+                    <p className="faction-name">FIB</p>
+                </div>
+                <div className="faction-card" onClick={() => navigate('/lspd')}>
+                    <img src={lspdLogo} alt="EMS" className="faction-logo" />
+                    <p className="faction-name">EMS</p>
+                </div>
+                <div className="faction-card" onClick={() => navigate('/lspd')}>
+                    <img src={lspdLogo} alt="GOV" className="faction-logo" />
+                    <p className="faction-name">GOV</p>
+                </div>
+                <div className="faction-card" onClick={() => navigate('/lspd')}>
+                    <img src={lspdLogo} alt="SASPA" className="faction-logo" />
+                    <p className="faction-name">SASPA</p>
                 </div>
             </div>
         </div>
